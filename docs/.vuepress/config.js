616d0ad4b18632c971e13ada0fcec85b15b45c95
wanghongjun <wanghongjun@u51.com>
2020-04-03 16:33:33 +0800

    feat(readme): go go go

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,42 +3,32 @@
 module.exports = {
     title: 'whjway',
     description: 'Blog',
-    theme: '@vuepress/theme-vue',
     themeConfig: {
         nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'Learn', link: '/learn/' },
-            // {
-            //     text: 'Languages',
-            //     items: [
-            //         { text: 'Chinese', link: '/language/chinese' },
-            //         { text: 'Japanese', link: '/language/japanese' }
-            //     ]
-            // },
-            // { text: 'External', link: 'https://google.com' }
+            { text: '主页', link: '/' },
+            // { text: 'Guide', link: '/guide/' },
+            { text: '资料', link: '/learn/' },
+            { text: 'Github', link: 'https://github.com/whjway' }
         ],
-        // sidebar: {
-        //     '/guide/': [
-        //         '/guide/',
-        //         '/guide/installation',
-        //         {
-        //             title: 'Basics',
-        //             collapsable: false,
-        //             children: [
-        //                 '/guide/prototyping',
-        //                 '/guide/creating-a-project',
-        //                 '/guide/plugins-and-presets',
-        //                 '/guide/cli-service'
-        //             ]
-        //         },
-        //         {
-        //             title: 'Group 2',
-        //             children: [
-        //                 '/guide/test'
-        //             ]
-        //         }
-        //     ]
-        // }
+        sidebar: {
+            '/learn/': [
+                '/learn/',
+                {
+                    title: 'vue相关',
+                    collapsable: false,
+                    children: [
+                        '/learn/installation'
+                    ]
+                },
+                {
+                    title: '工具',
+                    collapsable: false,
+                    children: [
+                        '/learn/markdown',
+                        '/learn/git'
+                    ]
+                },
+            ]
+        }
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,14 +1,16 @@
 ---
 home: true
-heroText: Hero 标题
-tagline: Hero 副标题
-actionText: 快速上手 →
+heroImage: /superman.jpg
+heroText: whjway
+tagline: Vue、前端知识点、小技巧
+actionText: 快速学习 →
+actionLink: /learn/
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You
+- title: 简洁
+  details: 知识点梳理、巩固，同时方便查阅。
+- title: 提升
+  details: 自我驱动，自我提升。
+- title: 技巧
+  details: 避坑指南。
+footer: MIT Licensed | Copyright © 2018-present whjway
 ---
